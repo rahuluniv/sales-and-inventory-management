db4d516bb341bdd19d445fbc03df36415bcc6e07
rahuluniv <rahulunivoffical@gmail.com>
2025-04-11 14:48:00 -0400

    Changes

--- InventoryMS/urls.py
+++ InventoryMS/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('transactions/', include('transactions.urls')),
     path('accounts/', include('accounts.urls')),
     path('invoice/', include('invoice.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('bills/', include('bills.urls'))
 ]
